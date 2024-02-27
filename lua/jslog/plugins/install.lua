2367909e0364fa2791bf062cca82b095c07fb087
js-jslog <josephsinfield@yahoo.com>
2024-02-27 16:51:04 +0000

    Add lsp using lsp-zero and mason

--- lua/jslog/plugins/install.lua
+++ lua/jslog/plugins/install.lua
@@ -23,5 +23,14 @@ require('lazy').setup({
   {
 	  'nvim-treesitter/nvim-treesitter',
 	  run = ':TSUpdate'
-  }
+  },
+  -- START LSP RELATED PLUGINS --
+  {'williamboman/mason.nvim'},
+  {'williamboman/mason-lspconfig.nvim'},
+  {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+  {'neovim/nvim-lspconfig'},
+  {'hrsh7th/cmp-nvim-lsp'},
+  {'hrsh7th/nvim-cmp'},
+  {'L3MON4D3/LuaSnip'},
+  -- END LSP RELATED PLUGINS --
 })
