2003aade4f5416163eb229716a60e8d0719723bc
js-jslog <josephsinfield@yahoo.com>
2024-02-27 16:55:57 +0000

    Add pane resizing key mappings

--- lua/jslog/remap.lua
+++ lua/jslog/remap.lua
@@ -9,6 +9,11 @@ vim.keymap.set('n', '<C-h>', '<C-w>h')
 vim.keymap.set('n', '<C-j>', '<C-w>j')
 vim.keymap.set('n', '<C-k>', '<C-w>k')
 vim.keymap.set('n', '<C-l>', '<C-w>l')
+vim.keymap.set("n", "<Right>", ":vertical resize +5<CR>", { silent = true })
+vim.keymap.set("n", "<Left>", ":vertical resize -5<CR>", { silent = true })
+vim.keymap.set("n", "<Up>", ":resize +5<CR>", { silent = true })
+vim.keymap.set("n", "<Up>", ":resize -5<CR>", { silent = true })
 
 vim.api.nvim_set_keymap('n', '<Leader>gp', ":execute ':!git push'<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<Leader>gl', ":execute ':!git pull'<CR>", { noremap = true, silent = true })
+
