33cd3b8780c030838a491fe4af7bea32c2360927
js-jslog <josephsinfield@yahoo.com>
2024-02-27 16:19:03 +0000

    Add git push and pull keybinds

--- lua/jslog/remap.lua
+++ lua/jslog/remap.lua
@@ -9,3 +9,6 @@ vim.keymap.set('n', '<C-h>', '<C-w>h')
 vim.keymap.set('n', '<C-j>', '<C-w>j')
 vim.keymap.set('n', '<C-k>', '<C-w>k')
 vim.keymap.set('n', '<C-l>', '<C-w>l')
+
+vim.api.nvim_set_keymap('n', '<Leader>gp', ":execute ':!git push'<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>gl', ":execute ':!git pull'<CR>", { noremap = true, silent = true })
