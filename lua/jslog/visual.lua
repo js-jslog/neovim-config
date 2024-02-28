b4ab1955a63d329fe3bfcc203676f254d5c0383e
Joseph Sinfield <Joseph.Sinfield@rullion.co.uk>
2024-02-28 09:25:42 +0000

    Add some config around buffer presentation

--- lua/jslog/visual.lua
+++ lua/jslog/visual.lua
@@ -1,3 +1,15 @@
 vim.cmd[[colorscheme tokyonight]]
 
 vim.opt.diffopt:append('vertical')
+
+vim.opt.guicursor = "" -- fat cursor
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
