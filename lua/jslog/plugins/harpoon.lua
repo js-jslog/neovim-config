2b358861e09966f536f43df27b000d4f70be7089
js-jslog <joseph.sinfield@yahoo.com>
2024-03-01 16:48:01 +0000

    Add harpoon shortcuts

--- lua/jslog/plugins/harpoon.lua
+++ lua/jslog/plugins/harpoon.lua
@@ -3,3 +3,5 @@ local ui = require('harpoon.ui')
 
 vim.keymap.set('n', '<Leader>a', mark.add_file)
 vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
+vim.keymap.set('n', '<C-p>', ui.nav_prev)
+vim.keymap.set('n', '<C-n>', ui.nav_next)
