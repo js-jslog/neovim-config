13b9a115dd0e54b4baa9ee2eaeddf93330fb2340
js-jslog <joseph.sinfield@yahoo.com>
2024-03-04 11:51:54 +0000

    Improve harpoon menu keybinding
    This is quicker and also frees up C-e for single line buffer scrolling.

--- lua/jslog/plugins/harpoon.lua
+++ lua/jslog/plugins/harpoon.lua
@@ -2,6 +2,6 @@ local mark = require('harpoon.mark')
 local ui = require('harpoon.ui')
 
 vim.keymap.set('n', '<Leader>a', mark.add_file)
-vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
+vim.keymap.set('n', '<Leader>o', ui.toggle_quick_menu)
 vim.keymap.set('n', '<C-p>', ui.nav_prev)
 vim.keymap.set('n', '<C-n>', ui.nav_next)
