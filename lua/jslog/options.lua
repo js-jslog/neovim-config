a8b173b1f09ea6a0700310495e2fec60c8fc398b
js-jslog <josephsinfield@yahoo.com>
2024-03-12 16:28:47 +0000

    Add a custom clipboard handler
    All the code for this commit was figured out in a neovim config file so
    while it's a fairly large commit, and it's not actually been tested as
    written here, at least you understand why.

--- lua/jslog/options.lua
+++ lua/jslog/options.lua
@@ -22,3 +22,57 @@ vim.opt.colorcolumn = '50,80,100'
 vim.opt.cursorcolumn = true
 
 vim.opt.updatetime = 50
+
+-- The following clipboard is for use with an editor
+-- running in a container and coupled with a strategy
+-- which uses the /dev/clipboard as an intermediary.
+-- This config specifically relies on a socat emitter
+-- to get the clipboard contents out to the host,
+-- but is agnostic about how the host is getting
+-- it's clipboard content in to the container.
+local function WriteToClipboard(clipcontents)
+    local clipboard_file = io.open('/dev/clipboard', 'w')
+    if clipboard_file then
+        for index, clipline in pairs(clipcontents) do
+            if index > 1 then
+                clipboard_file:write('\n')
+            end
+            clipboard_file:write(clipline)
+        end
+        clipboard_file:close()
+    else
+        print('Error: unable to open /dev/clipboard for writing.')
+    end
+end
+
+local function EmitClipboard()
+    local script_path = '/development-env/socat-emitter-container.sh'
+    local command = 'sh ' .. script_path
+
+    vim.fn.system(command)
+
+    if vim.v.shell_error == 0 then
+        print('Clipboard emitted successfully')
+    else
+        print('Error: Clipboard not emitted')
+    end
+end
+
+vim.g.clipboard = {
+    name = 'container-compatible-clipboard',
+    copy = {
+        ['+'] = function(clipcontents)
+            WriteToClipboard(clipcontents)
+            EmitClipboard()
+        end,
+        ['*'] = function(clipcontents)
+            WriteToClipboard(clipcontents)
+            EmitClipboard()
+        end,
+    },
+    paste = {
+        ['+'] = 'cat /dev/clipboard',
+        ['*'] = 'cat /dev/clipboard',
+    },
+    cache_enabled = 1
+}
