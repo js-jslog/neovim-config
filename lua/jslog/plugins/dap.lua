c8ec41eb99e2b41ec724a0a3027de6be4af7dd31
sinfiej <joseph.sinfield@rullion.co.uk>
2024-04-11 10:05:21 +0000

    Remove unused adapters from dap config

--- lua/jslog/plugins/dap.lua
+++ lua/jslog/plugins/dap.lua
@@ -4,7 +4,8 @@ require("dap-vscode-js").setup({
   debugger_path = vim.fn.resolve(vim.fn.stdpath("data") .. "/lazy/vscode-js-debug/"), -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
   -- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
   -- debugger_cmd = { vim.fn.resolve(vim.fn.stdpath("data") .. "/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js") }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
-  adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost', 'chrome' }, -- which adapters to register in nvim-dap
+  -- adapters = { 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost', 'chrome' }, -- which adapters to register in nvim-dap
+  adapters = { 'pwa-node', 'pwa-chrome' }, -- which adapters to register in nvim-dap
   -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
   -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
   -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
@@ -68,11 +69,8 @@ end
 local dap_vscode = require('dap.ext.vscode')
 dap_vscode.json_decode = require('overseer.json').decode
 dap_vscode.load_launchjs(nil, {
-  ['chrome'] = js_based_languages,
-  ['node'] = js_based_languages,
   ['pwa-node'] = js_based_languages,
   ['pwa-chrome'] = js_based_languages,
-  ['node-terminal'] = js_based_languages,
 })
 
 vim.keymap.set('n', '<Leader>db', ':lua require"dap".toggle_breakpoint()<CR>')
