c998404705cd7e315c02b326cad491a54245b9fa
sinfiej <joseph.sinfield@rullion.co.uk>
2024-04-23 10:29:57 +0000

    Format file slightly
    I'm not sure where this formatting came from actually. Probably it's
    from lua_ls somehow. I need to add stylua for a more complete formatting
    experience.

--- lua/jslog/plugins/lsp.lua
+++ lua/jslog/plugins/lsp.lua
@@ -1,15 +1,16 @@
 local lsp_zero = require('lsp-zero')
 
+---@diagnostic disable-next-line: unused-local
 lsp_zero.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
+  lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
   -- Requirements defined at https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-  ensure_installed = {'tsserver', 'eslint', 'lua_ls'},
+  ensure_installed = { 'tsserver', 'eslint', 'lua_ls' },
   handlers = {
     lsp_zero.default_setup,
   },
