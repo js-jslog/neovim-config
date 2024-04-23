6ad0f98cc8825a47aa79405704583149b59a2ff6
sinfiej <joseph.sinfield@rullion.co.uk>
2024-04-23 14:06:00 +0000

    Reintroduce necessary config lost during merge
    Just due to personal incompetence, and a little to do with using
    lazygit's merge edit mode for the first time.

--- lua/jslog/plugins/install.lua
+++ lua/jslog/plugins/install.lua
@@ -1,3 +1,16 @@
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
 require("lazy").setup({
   -- START THE DEPENDENCY ONLY PLUGINS
   -- as far as I'm aware I don't think
