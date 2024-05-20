7f6b50652e4c2d0c815ec5c55852267ab4a9647b
sinfiej <joseph.sinfield@rullion.co.uk>
2024-05-20 14:38:17 +0000

    Localise the dependencies even further
    Experimenting with nesting the dependency definitions. I still need to
    define the tag version of shared dependencies outside of the the
    nesting. I might be able to improve that later with variables.

--- lua/jslog/plugins/install.lua
+++ lua/jslog/plugins/install.lua
@@ -26,6 +26,14 @@ require("lazy").setup({
 	{ "nvim-lua/plenary.nvim", tag = "v0.1.4" },
 	{ "nvim-tree/nvim-web-devicons", tag = "v0.100" },
 	{ "williamboman/mason.nvim", tag = "v1.10.0" },
+	-- treesitter is actually only required as a dep by one
+	-- other plugin, but it needs to persist in it's own right
+	-- so if that plugin is removed, this should stay.
+	{
+		"nvim-treesitter/nvim-treesitter",
+		tag = "v0.9.2",
+		run = ":TSUpdate",
+	},
 	-----------------------------------------------------------
 	--.......................................................--
 	--.......................................................--
@@ -46,24 +54,19 @@ require("lazy").setup({
 	--....................................--
 	----------------------------------------
 	{
-		"williamboman/mason-lspconfig.nvim",
-		tag = "v1.29.0",
-		dependencies = { "williamboman/mason.nvim" },
-	},
-	{ "neovim/nvim-lspconfig", tag = "v0.1.7" },
-	{ "hrsh7th/cmp-nvim-lsp", version = "39e2eda76828d88b773cc27a3f61d2ad782c922d" },
-	{ "hrsh7th/nvim-cmp", version = "5260e5e8ecadaf13e6b82cf867a909f54e15fd07" },
-	{ "L3MON4D3/LuaSnip", tag = "v2.3.0" },
-	{
 		"VonHeikemen/lsp-zero.nvim",
 		branch = "v3.x",
 		-- version = "f12d50716e8e59ea9f5cf484eac6968c33a95917" TODO: replace previous line with this when ready
 		dependencies = {
-			"williamboman/mason-lspconfig.nvim",
-			"neovim/nvim-lspconfig",
-			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/nvim-cmp",
-			"L3MON4D3/LuaSnip",
+			{
+				"williamboman/mason-lspconfig.nvim",
+				tag = "v1.29.0",
+				dependencies = { "williamboman/mason.nvim" },
+			},
+			{ "neovim/nvim-lspconfig", tag = "v0.1.7" },
+			{ "hrsh7th/cmp-nvim-lsp", version = "39e2eda76828d88b773cc27a3f61d2ad782c922d" },
+			{ "hrsh7th/nvim-cmp", version = "5260e5e8ecadaf13e6b82cf867a909f54e15fd07" },
+			{ "L3MON4D3/LuaSnip", tag = "v2.3.0" },
 		},
 	},
 	-----------------------------------
@@ -72,16 +75,14 @@ require("lazy").setup({
 	--...............................--
 	-----------------------------------
 	{
-		"nvimtools/none-ls.nvim",
-		version = "88821b67e6007041f43b802f58e3d9fa9bfce684",
-	},
-	{
 		"jay-babu/mason-null-ls.nvim",
 		tag = "v2.6.0",
 		event = { "BufReadPre", "BufNewFile" },
 		dependencies = {
-			"williamboman/mason.nvim",
-			"nvimtools/none-ls.nvim",
+			{
+				"williamboman/mason.nvim" --[[version = see shared dependencies at top of file --]],
+			},
+			{ "nvimtools/none-ls.nvim", version = "88821b67e6007041f43b802f58e3d9fa9bfce684" },
 		},
 	},
 	-----------------------------------
@@ -104,24 +105,35 @@ require("lazy").setup({
 		tag = "0.7.0",
 	},
 	{
-		"stevearc/overseer.nvim",
-		tag = "v1.3.1",
-		opts = {},
-	},
-	{
-		"microsoft/vscode-js-debug",
-		tag = "v1.88.0",
-		build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
-	},
-	{
 		"mxsdev/nvim-dap-vscode-js",
 		tag = "v1.1.0",
-		dependencies = { "mfussenegger/nvim-dap", "microsoft/vscode-js-debug", "stevearc/overseer.nvim" },
+		dependencies = {
+			{
+				"mfussenegger/nvim-dap", --[[tag = see shared dependency a few lines up --]]
+			},
+			{
+				"microsoft/vscode-js-debug",
+				tag = "v1.88.0",
+				build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
+			},
+			{
+				"stevearc/overseer.nvim",
+				tag = "v1.3.1",
+				opts = {},
+			},
+		},
 	},
 	{
 		"rcarriga/nvim-dap-ui",
 		tag = "v4.0.0",
-		dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+		dependencies = {
+			{
+				"mfussenegger/nvim-dap" --[[tag = see shared dependency a few lines up --]],
+			},
+			{
+				"nvim-neotest/nvim-nio" --[[tag = see shared dependency at top of file --]],
+			},
+		},
 	},
 	----------------------------------------
 	--....................................--
@@ -133,26 +145,36 @@ require("lazy").setup({
 	--........ START TEST PLUGINS ........--
 	--....................................--
 	----------------------------------------
-	{ "antoinemadec/FixCursorHold.nvim", version = "1900f89dc17c603eec29960f57c00bd9ae696495" }, -- Claims not to be required at https://github.com/antoinemadec/FixCursorHold.nvim but it is. See https://github.com/antoinemadec/FixCursorHold.nvim/issues/13
-	{ "marilari88/neotest-vitest", tag = "v0.2.0" },
-	{ "nvim-neotest/neotest-jest", version = "514fd4eae7da15fd409133086bb8e029b65ac43f" },
-	{ "nvim-neotest/neotest-go", version = "6a2f996d89fe4631942e035b1c114544ee045043" },
-	-- { "folke/neodev.nvim", tag = "v2.5.2", opts = {} },
 	{
 		"nvim-neotest/neotest",
 		tag = "v5.1.0",
 		dependencies = {
 			-- first the required deps
-			"nvim-neotest/nvim-nio",
-			"nvim-lua/plenary.nvim",
-			"antoinemadec/FixCursorHold.nvim", -- Claims not to be required at https://github.com/antoinemadec/FixCursorHold.nvim but it is. See https://github.com/antoinemadec/FixCursorHold.nvim/issues/13
-			"nvim-treesitter/nvim-treesitter",
+			{
+				"nvim-neotest/nvim-nio", --[[tag = see shared dependency at top of file --]]
+			},
+			{
+				"nvim-lua/plenary.nvim", --[[tag = see shared dependency at top of file --]]
+			},
+			{
+				"antoinemadec/FixCursorHold.nvim",
+				version = "1900f89dc17c603eec29960f57c00bd9ae696495",
+			}, -- Claims not to be required at https://github.com/antoinemadec/FixCursorHold.nvim but it is. See https://github.com/antoinemadec/FixCursorHold.nvim/issues/13
+			{
+				"nvim-treesitter/nvim-treesitter", --[[tag = see shared dependency at top of file --]]
+			},
 			-- the optional one
-			-- "folke/neodev.nvim",
+			-- { "folke/neodev.nvim", tag = "v2.5.2", opts = {} },
 			-- then adapters
-			"marilari88/neotest-vitest",
-			"nvim-neotest/neotest-jest",
-			"nvim-neotest/neotest-go",
+			{ "marilari88/neotest-vitest", tag = "v0.2.0" },
+			{
+				"nvim-neotest/neotest-jest",
+				version = "514fd4eae7da15fd409133086bb8e029b65ac43f",
+			},
+			{
+				"nvim-neotest/neotest-go",
+				version = "6a2f996d89fe4631942e035b1c114544ee045043",
+			},
 		},
 	},
 	----------------------------------------
@@ -166,11 +188,6 @@ require("lazy").setup({
 	--....................................--
 	----------------------------------------
 	{
-		"nvim-treesitter/nvim-treesitter",
-		tag = "v0.9.2",
-		run = ":TSUpdate",
-	},
-	{
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.5",
 		dependencies = { "nvim-lua/plenary.nvim" },
@@ -178,25 +195,29 @@ require("lazy").setup({
 	{ "theprimeagen/harpoon", version = "ccae1b9bec717ae284906b0bf83d720e59d12b91" },
 	{ "tpope/vim-fugitive", version = "4f59455d2388e113bd510e85b310d15b9228ca0d" },
 	{ "kdheepak/lazygit.nvim", version = "0ada6c6e7e138df92f5009b6952f4ac41248305a" },
-	----------------------------------------
-	--....................................--
-	--..... WARNING ....... WARNING ......--
-	--....................................--
-	----------------------------------------
-	-- Included to allow `Trouble workspace_diagnostics` to show results for non-buffered files.
-	-- The autor warns that this might come with some risk so I should keep my eyes open.
-	{ "artemave/workspace-diagnostics.nvim", version = "429174d53652dbef56868a2cbeb8b28cb5d44fdd" },
-	----------------------------------------
-	--....................................--
-	--. END WARNING ... END WARNING ......--
-	--....................................--
-	----------------------------------------
 	{
 		"folke/trouble.nvim",
 		tag = "v2.10.0",
 		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-			"artemave/workspace-diagnostics.nvim",
+			{
+				"nvim-tree/nvim-web-devicons", --[[tag = see shared dependency at top of file --]]
+			},
+			{
+				----------------------------------------
+				--....................................--
+				--..... WARNING ....... WARNING ......--
+				--....................................--
+				----------------------------------------
+				-- Included to allow `Trouble workspace_diagnostics` to show results for non-buffered files.
+				-- The autor warns that this might come with some risk so I should keep my eyes open.
+				"artemave/workspace-diagnostics.nvim",
+				version = "429174d53652dbef56868a2cbeb8b28cb5d44fdd",
+				----------------------------------------
+				--....................................--
+				--. END WARNING ... END WARNING ......--
+				--....................................--
+				----------------------------------------
+			},
 		},
 	},
 	{ "mbbill/undotree", version = "56c684a805fe948936cda0d1b19505b84ad7e065" },
@@ -224,16 +245,20 @@ require("lazy").setup({
 	{
 		"nvim-lualine/lualine.nvim",
 		tag = "compat-nvim-0.6",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
+		dependencies = {
+			{
+				"nvim-tree/nvim-web-devicons", --[[tag = see shared dependency at top of file --]]
+			},
 	},
-	{ "MunifTanjim/nui.nvim", tag = "0.3.0" },
-	{ "rcarriga/nvim-notify", tag = "v3.13.4" },
 	{
 		"folke/noice.nvim",
 		tag = "v2.0.1",
 		event = "VeryLazy",
 		opts = {},
-		dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
+		dependencies = {
+			{ "MunifTanjim/nui.nvim", tag = "0.3.0" },
+			{ "rcarriga/nvim-notify", tag = "v3.13.4" },
+		},
 	},
 	{
 		"stevearc/dressing.nvim",
